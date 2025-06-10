4fe63dbb40506172366d0f01ac08407456ba076d
anggierz <anggie.rizo.zambrano@gmail.com>
2025-06-10 22:07:41 +0200

    (feat): add different menus if browser or mobile

--- src/app/components/tooltip/tooltip.component.ts
+++ src/app/components/tooltip/tooltip.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'app-tooltip',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatMenuModule],
   templateUrl: './tooltip.component.html',
   styleUrl: './tooltip.component.css'
 })
