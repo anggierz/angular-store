89c1eacf2aa949714dae943823e15c15dc3579f0
anggierz <anggie.rizo.zambrano@gmail.com>
2025-06-15 17:34:34 +0200

    getAllProducts + getProductById in service

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../interfaces/product.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+
+  private readonly baseUrl = 'https://fakestoreapi.com/products';
+
+  constructor(private http: HttpClient) { }
+
+  getAllProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl);
+  }
+
+  getProductById(id: number): Observable<Product> {
+    return this.http.get<Product>(`${this.baseUrl}/${id}`);
+  }
+  
 }
